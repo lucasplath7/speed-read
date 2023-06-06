7ab1077e1b2f438974ecec480f07b88c98cc88a1
Lucas Plath <lucas.plath@gmail.com>
2023-06-06 18:39:28 -0400

    dont ignore node modules in npm

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,41 @@
+"use strict";
+
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+require("core-js/modules/es.symbol.js");
+require("core-js/modules/es.symbol.description.js");
+require("core-js/modules/es.object.to-string.js");
+require("core-js/modules/es.symbol.iterator.js");
+require("core-js/modules/es.array.iterator.js");
+require("core-js/modules/es.string.iterator.js");
+require("core-js/modules/web.dom-collections.iterator.js");
+require("core-js/modules/es.function.name.js");
+require("core-js/modules/es.array.from.js");
+require("core-js/modules/es.symbol.to-primitive.js");
+require("core-js/modules/es.date.to-primitive.js");
+require("core-js/modules/es.number.constructor.js");
+require("core-js/modules/es.object.keys.js");
+require("core-js/modules/es.array.filter.js");
+require("core-js/modules/es.object.get-own-property-descriptor.js");
+require("core-js/modules/web.dom-collections.for-each.js");
+require("core-js/modules/es.object.get-own-property-descriptors.js");
+require("core-js/modules/es.weak-map.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = SpeedRead;
+require("core-js/modules/es.regexp.exec.js");
+require("core-js/modules/es.regexp.test.js");
+require("core-js/modules/es.string.replace.js");
+require("core-js/modules/es.string.split.js");
+require("core-js/modules/es.error.cause.js");
+require("core-js/modules/es.math.trunc.js");
+require("core-js/modules/es.array.join.js");
+require("core-js/modules/web.timers.js");
+require("core-js/modules/es.array.slice.js");
+var _react = _interopRequireWildcard(require("react"));
+require("./index.css");
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -10,8 +47,6 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-import React, { useEffect, useState } from 'react';
-import './index.css';
 var isEmoji = function isEmoji(word) {
   return /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g.test(word);
 };
@@ -28,7 +63,7 @@ var charTimeout = function charTimeout(text, wpm) {
   var totalTime = Math.trunc(words.length / wpm * ONE_MINUTE);
   return totalTime / words.join('').length;
 };
-export default function SpeedRead(props) {
+function SpeedRead(props) {
   var initialState = {
     charTempo: charTimeout(props.text, props.wpm),
     currentWordIndex: 0,
@@ -41,20 +76,20 @@ export default function SpeedRead(props) {
     textUpdated: false,
     timeout: null
   };
-  var _useState = useState(initialState),
+  var _useState = (0, _react.useState)(initialState),
     _useState2 = _slicedToArray(_useState, 2),
     state = _useState2[0],
     setState = _useState2[1];
-  useEffect(function () {
+  (0, _react.useEffect)(function () {
     handleChangeText();
   }, [props.text]);
-  useEffect(function () {
+  (0, _react.useEffect)(function () {
     props.playing ? start() : stop();
   }, [props.playing]);
-  useEffect(function () {
+  (0, _react.useEffect)(function () {
     handleUpdateWPM();
   }, [props.wpm]);
-  useEffect(function () {
+  (0, _react.useEffect)(function () {
     if (state.playing && props.playing && state.text === props.text && state.wpm === props.wpm && state.currentWordIndex < state.words.length && !state.timeout) {
       setState(_objectSpread(_objectSpread({}, state), {}, {
         timeout: setTimeout(displayNextWord, state.currentWordIndex === 0 ? 1000 : getNextWordTimeout())
@@ -110,17 +145,17 @@ export default function SpeedRead(props) {
   var currentWordIndex = state.currentWordIndex;
   var word = currentWordIndex !== -1 && state.words[currentWordIndex];
   var highlightIndex = word && calcHighlightPoint(word);
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement("div", {
     className: "container"
-  }, word && /*#__PURE__*/React.createElement("div", {
+  }, word && /*#__PURE__*/_react.default.createElement("div", {
     className: "spritz"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "leftSide"
-  }, highlightIndex !== -1 && word.slice(0, highlightIndex)), /*#__PURE__*/React.createElement("div", {
+  }, highlightIndex !== -1 && word.slice(0, highlightIndex)), /*#__PURE__*/_react.default.createElement("div", {
     className: "highlight"
-  }, highlightIndex === -1 ? word : word[highlightIndex]), /*#__PURE__*/React.createElement("div", {
+  }, highlightIndex === -1 ? word : word[highlightIndex]), /*#__PURE__*/_react.default.createElement("div", {
     className: "rightSide"
-  }, highlightIndex !== -1 && word.slice(highlightIndex + 1))), word && state.startTimeout && /*#__PURE__*/React.createElement("div", {
+  }, highlightIndex !== -1 && word.slice(highlightIndex + 1))), word && state.startTimeout && /*#__PURE__*/_react.default.createElement("div", {
     style: {
       transition: "transform linear ".concat(state.startTimeout, "ms")
     },
