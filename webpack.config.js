03048594f0fb0bfb76059cd6b6c90afc03a14b86
Lucas Plath <lucas.plath@gmail.com>
2023-06-07 17:26:55 -0400

    attempt webpack build

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+}
